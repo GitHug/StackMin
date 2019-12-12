3f26f690216778c6bb64a00e4cf93c1f03981f22
Fredrik Mäkilä <fredrik.makila@gmail.com>
2019-12-12 22:04:57 +0000

    Use additional stack instead of using objects to track min values

--- index.js
+++ index.js
@@ -1,34 +1,23 @@
 class StackMin {
   constructor () {
     this.stack = []
+    this.minStack = []
   }
 
   push (value) {
-    const top = this.peek()
-
-    if (top) {
-      const { min: previousMin } = top
-
-      const min = Math.min(value, previousMin)
-
-      this.stack.push({ value, min })
-    } else {
-      this.stack.push({ value, min: value })
-    }
+    if (value <= this.min()) this.minStack.push(value)
+    this.stack.push(value)
   }
 
   pop () {
-    return this.stack.pop()
+    const top = this.stack.pop()
+    if (top === this.min()) this.minStack.pop()
+    return top
   }
 
   min () {
-    const top = this.peek()
-
-    return top && top.min
-  }
-
-  peek () {
-     return this.stack[this.stack.length - 1] 
+    if (!this.minStack.length) return Number.MAX_VALUE
+    return this.minStack[this.minStack.length - 1]
   }
 }
 
